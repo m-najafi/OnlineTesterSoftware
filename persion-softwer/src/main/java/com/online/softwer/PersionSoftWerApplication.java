3cff6e2851661b4a15a46a1e9dd329b46674a36a
m-najafi <m.najafi68@outlook.com>
2020-10-24 13:18:32 +0330

    create project PersionSoftwer

---
+++ persion-softwer/src/main/java/com/online/softwer/PersionSoftWerApplication.java
@@ -0,0 +1,13 @@
+package com.online.softwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersionSoftWerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersionSoftWerApplication.class, args);
+    }
+
+}
---
+++ persion-softwer/src/test/java/com/online/softwer/PersionSoftWerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.online.softwer;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PersionSoftWerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        String test="";
+        Assertions.assertNotNull(test);
+    }
+
+}
